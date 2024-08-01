80de6781f93568a33957e73f04d33d7d48e962d4
Hari-1202"git config --global user.name Hari-1202git config --global user.name Hari-1202 <hariharandhanraj0203@gmail.com>
2024-08-01 16:49:32 +0530

    Checking caching

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1 style={{backgroundColor:"green"}}>Learn  & Master GitHub Actions</h1>
+        <h1 style={{backgroundColor:"green"}}>Learn  & Master GitHub Actions & Caching</h1>
       </header>
       <MainContent />
     </>
